967e6cacb0cf7c43e215e624f4c43bfbbba015c3
AAYUSH BISHNOI <aayushkaswan79@gmail.com>
2023-09-04 11:34:08 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['dist/index.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'gilroy':["Gilroy-bold"],
+        'play':["Gilroy-light"]
+      }
+    },
+  },
+  plugins: [],
+}
+
